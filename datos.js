e50103d21b9188914cd37a7fa4d90636b7178732
Coche77 <127133878+Coche77@users.noreply.github.com>
2026-02-25 23:59:01 -0600

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,20 @@
+const CLIENTES_REGISTRADOS = [
+    {
+        nombre: "GERARDO VALDEZ",
+        proyecto: "SMOKE HOUSE",
+        ubicacion: "APODACA, N.L.",
+        introduccion: "Estimado Gerardo; Atendiendo su amable solicitud, le presentamos la propuesta técnica y económica para el proyecto SMOKE HOUSE ubicado en Apodaca, N.L., que amablemente nos consultó."
+    },
+    {
+        nombre: "FELIPE MIER MARTINEZ",
+        proyecto: "ARENA DOMO",
+        ubicacion: "SANTA CATARINA, N.L.",
+        introduccion: "Estimado Sr. Felipe Mier; Atendiendo su Amable Solicitud le Presentamos la Propuesta de la Adecuacion de Cocina y Closet de su Departamento ubicado en Arena domo en Santa catarina,N.L. que Amablemente nos Solicito"
+    },
+    {
+        nombre: "IG ANUNCIOS SA DE CV",
+        proyecto: "PROYECTO IG ANUNCIOS",
+        ubicacion: "MONTERREY, N.L.",
+        introduccion: "A la atención del equipo de IG ANUNCIOS SA DE CV; Presentamos a continuación la propuesta detallada según los requerimientos conversados previamente."
+    }
+];
